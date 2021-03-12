79b6b3d3a1cdf975c3092fe29acfddac0ccbc25e
ashraf mohammad <7367600+AhnafCodes@users.noreply.github.com>
2021-03-12 11:56:38 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import SessionStorageUtil from "./SessionStorageUtils.js";
+import LocalStorageUtil from "./LocalStorageUtils.js";
+
+export default { LocalStorageUtil,SessionStorageUtil };
+  
+  
+  
